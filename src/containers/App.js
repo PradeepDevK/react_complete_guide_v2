54861bf9468e99d7d5f0aa5130f7348e444ced2c
ACCIONLABS\Al1430 <pradeepjack555@gmail.com>
2020-02-20 15:44:55 +0530

    Component update lifecycle(for state changes)

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,6 +33,16 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('[App.js] componentDidUpdate');
+  }
+  
+
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
