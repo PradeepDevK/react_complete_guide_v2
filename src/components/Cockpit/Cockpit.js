84fb1cabf08540d0f4ec8f3a88ef6d1449d14575
ACCIONLABS\Al1430 <pradeepjack555@gmail.com>
2020-02-21 17:54:53 +0530

    Using the Context API

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 
 import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
 
 const cockpit = ( props ) => {
 
@@ -51,7 +52,7 @@ const cockpit = ( props ) => {
                 ref = {toggleBtnRef}
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
-            <button onClick={props.login}>Log in</button>
+            <AuthContext.Consumer>{context => <button onClick={context.login}>Log in</button>}</AuthContext.Consumer>
         </div>
     );
 };
