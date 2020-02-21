3524021c0f6a17bbe2f7ed09ad5fce3468497ada
ACCIONLABS\Al1430 <pradeepjack555@gmail.com>
2020-02-21 14:53:11 +0530

    Passing Unknown Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.css';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
 
@@ -18,4 +19,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
